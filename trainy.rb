2116f1ae5694d72b9ee983ffc5ad44ddb081376f
wonderchook <wonderchook@ubuntu.(none)>
2011-02-21 12:46:42 -0800

    adding API key

--- trainy.rb
+++ trainy.rb
@@ -1,1 +1,18 @@
+require 'yaml'
 
+
+class Trainy
+
+  def initialize
+	config = YAML.load_file("wmata.yaml")
+	@api_key = config["config"]["metro_api"]
+  end
+  
+  def next_train()
+  #http://api.wmata.com/StationPrediction.svc/json/GetPrediction/A10,A11?api_key=YOUR_API_KEY
+  
+  end
+
+
+
+end
